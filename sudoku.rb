7b7cbb4d90c1cf9595bfc611cdce5139b08da7e7
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-11-09 14:16:14 -0500

    level-2 fix

--- sudoku.rb
+++ sudoku.rb
@@ -1,4 +1,5 @@
 require_relative "board"
+require 'byebug'
 
 class SudokuGame
   def self.from_file(filename)
@@ -30,6 +31,7 @@ class SudokuGame
 
   def get_val
     val = nil
+    # debugger
     until val && valid_val?(val)
       puts "Please enter a value between 1 and 9 (0 to clear the tile)"
       print "> "
@@ -48,8 +50,9 @@ class SudokuGame
 
   def play_turn
     board.render
-    val = get_pos
-    pos = get_val
+    # switched pos and val values
+    pos = get_pos
+    val = get_val
     board[pos] = val
   end
 
